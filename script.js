a78e6949c1e7e6e28e9b1ba74e64e573274ff87e
Enes Işık <30756694+isikenes@users.noreply.github.com>
2020-12-04 12:42:08 +0300

    adding lyrics part

--- script.js
+++ script.js
@@ -22,3 +22,41 @@ $(function() {
         event.preventDefault();
     });
 });
+
+var commentSlider = {
+    config: {
+      container: $("#quote")
+    },
+  
+    init: function(config){
+      if(config && typeof config == "object"){
+        $.extend(commentSlider.config, config);
+      }
+  
+      //caching dom elements
+      //wrapper
+      commentSlider.$container = commentSlider.config.container;
+  
+      //first p tag within module wrapper
+      commentSlider.$firstParagraph = commentSlider.$container.find(
+        "p:first-child"
+      );
+  
+      //setting first paragraph tag with .active class
+      commentSlider.$firstParagraph.addClass("activeText");
+    }
+  };
+  
+  //initializes the entire thing by calling the init function
+  $(document).ready(commentSlider.init);
+  
+  $(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+      autoPlay: 4000,
+      slideSpeed: 1000,
+      navigation: false,
+      pagination: true,
+      singleItem: true,
+      dots: false
+    });
+  });
