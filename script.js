7fbd0fce517c69f1b47f731d546a8aba6e4e7b0e
Enes Işık <30756694+isikenes@users.noreply.github.com>
2020-12-06 12:05:12 +0300

    added background music

--- script.js
+++ script.js
@@ -60,3 +60,25 @@ var commentSlider = {
       dots: false
     });
   });
+
+  function pauseMusic(){
+
+    var audioPlayer = document.getElementById('audio-player');
+    var audioContainer = $('#music-container');
+  
+    audioPlayer.pause();
+    audioContainer.addClass("music-player--disabled");
+  
+    console.log("pause music");
+  }
+  
+  function playMusic(){
+  
+    var audioPlayer = document.getElementById('audio-player');
+    var audioContainer = $('#music-container');
+  
+    audioPlayer.play();
+    audioContainer.removeClass("music-player--disabled");
+  
+    console.log("play music");
+  }
